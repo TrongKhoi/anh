9d696a0a81b1a205337c0590ef57b057fece7599
TrongKhoi <40121111+TrongKhoi@users.noreply.github.com>
2019-04-12 13:42:54 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const config = require("./config.json");
+const oid = process.env.OID
 
 client.on("guildCreate", guild => {
   // This event triggers when the bot joins a guild.
@@ -41,7 +42,7 @@ client.on("message", async message => {
     m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
   }
   
-  if(command === "say") {
+  if(command === "say" && message.author.id == OID) {
     // makes the bot say something and delete the message. As an example, it's open to anyone to use. 
     // To get the "message" itself we join the `args` back into a string with spaces: 
     const sayMessage = args.join(" ");
