3c526a39d0bb301d9296fee2a2cd4336a8735ac4
TrongKhoi <40121111+TrongKhoi@users.noreply.github.com>
2019-04-12 13:24:49 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,47 +1,118 @@
+// Load up the discord.js library
 const Discord = require("discord.js");
+
+// This is your client. Some people call it `bot`, some people call it `self`, 
+// some might call it `cootchie`. Either way, when you see `client.something`, or `bot.something`,
+// this is what we're refering to. Your client.
 const client = new Discord.Client();
+
+// Here we load the config.json file that contains our token and our prefix values. 
 const config = require("./config.json");
 
-client.on("ready", () => {
- console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
-  client.user.setActivity(`Serving ${client.guilds.size} servers`);
-});
+
+//client.on("ready", () => {
+  // This event will run if the bot starts, and logs in, successfully.
+  //console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
+  // Example of changing the bot's playing game to something useful. `client.user` is what the
+  // docs refer to as the "ClientUser".
+  //client.user.setActivity(`Serving ${client.guilds.size} servers`);
+//});
+
 
 client.on("guildCreate", guild => {
+  // This event triggers when the bot joins a guild.
   console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
   client.user.setActivity(`Serving ${client.guilds.size} servers`);
 });
   client.on("guildDelete", guild => {
+  // this event triggers when the bot is removed from a guild.
   console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
   client.user.setActivity(`Serving ${client.guilds.size} servers`);
 });
 
+
 client.on("message", async message => {
+  // This event will run on every single message received, from any channel or DM.
+  
+  // It's good practice to ignore other bots. This also makes your bot ignore itself
+  // and not get into a spam loop (we call that "botception").
   if(message.author.bot) return;
+  
+  // Also good practice to ignore any message that does not start with our prefix, 
+  // which is set in the configuration file.
   if(message.content.indexOf(config.prefix) !== 0) return;
+  
+  // Here we separate our "command" name, and our "arguments" for the command. 
+  // e.g. if we have the message "+say Is this the real life?" , we'll get the following:
+  // command = say
+  // args = ["Is", "this", "the", "real", "life?"]
   const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
   
+  // Let's go with a few common example commands! Feel free to delete or change those.
+  
   if(command === "ping") {
+    // Calculates ping between sending a message and editing it, giving a nice round-trip latency.
+    // The second ping is an average latency between the bot and the websocket server (one-way, not round-trip)
     const m = await message.channel.send("Ping?");
     m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
   }
   
   if(command === "say") {
+    // makes the bot say something and delete the message. As an example, it's open to anyone to use. 
+    // To get the "message" itself we join the `args` back into a string with spaces: 
     const sayMessage = args.join(" ");
+    // Then we delete the command message (sneaky, right?). The catch just ignores the error with a cute smiley thing.
     message.delete().catch(O_o=>{}); 
+    // And we get the bot to say the thing: 
     message.channel.send(sayMessage);
   }
-	
+ 
+//Cau lenh kick nay rat nguy hiem
+  //if(command === "kick") {
+  //let member = message.mentions.members.first();
+  //let reason = args.slice(1).join(" ");
+  //member.kick(reason);
+  //}
+
+  
+  //if(command === "ban") {
+    // Most of this command is identical to kick, except that here we'll only let admins do it.
+    // In the real world mods could ban too, but this is just an example, right? ;)
+    //if(!message.member.roles.some(r=>["Administrator"].includes(r.name)) )
+      //return message.reply("Sorry, you don't have permissions to use this!");
+    
+    //let member = message.mentions.members.first();
+    //if(!member)
+      //return message.reply("Please mention a valid member of this server");
+   //if(!member.bannable) 
+      //return message.reply("I cannot ban this user! Do they have a higher role? Do I have ban permissions?");
+
+    //let reason = args.slice(1).join(' ');
+    //if(!reason) reason = "No reason provided";
+    
+    //await member.ban(reason)
+      //.catch(error => message.reply(`Sorry ${message.author} I couldn't ban because of : ${error}`));
+    //message.reply(`${member.user.tag} has been banned by ${message.author.tag} because: ${reason}`);
+  //}
+  
   if(command === "delete") {
+    // This command removes all messages from all users in the channel, up to 100.
+    
+    // get the delete count, as an actual number.
     const deleteCount = parseInt(args[0], 10);
+    
+    // Ooooh nice, combined conditions. <3
     if(!deleteCount || deleteCount < 2 || deleteCount > 100)
       return message.reply("Please provide a number between 2 and 100 for the number of messages to delete");
+    
+    // So we get our messages, and delete them. Simple enough, right?
     const fetched = await message.channel.fetchMessages({limit: deleteCount});
     message.channel.bulkDelete(fetched)
       .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
   }
 });
+
 
 client.on('message', msg => {
   if (msg.content === 'Hi') { msg.reply('Hi');}
@@ -54,11 +125,15 @@ client.on('message', msg => {
   if (msg.content === "tenk") { msg.reply('gọi cc');}
 });
 
+
 client.on('message', message => {
+  // If the message is "avatar"
   if (message.content === 'avatar') {
+    // Send the user's avatar URL
     message.reply(message.author.avatarURL);
   }
 });
+
 
 const embed = new Discord.RichEmbed()
   .setTitle("**Đây là info về người bạn xin**")
@@ -89,6 +164,7 @@ const embed = new Discord.RichEmbed()
 
 
 client.on("message", (message) => {
+	
   if (message.content.startsWith("meo meo")) {message.channel.send("Gâu gâu");}
   if (message.content.startsWith("em thích ai")) {message.channel.send("em thích trai kỹ thuật, họ mạnh mẽ và đầy nam tính");}
   if (message.content.startsWith("info")) {message.channel.send({embed});}
