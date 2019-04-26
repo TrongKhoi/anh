5f1a20e7efab9e68d5a737b45f5f7aa3e0c8b0db
TrongKhoi <40121111+TrongKhoi@users.noreply.github.com>
2019-04-26 18:05:22 +0700

    Update app.js

--- app.js
+++ app.js
@@ -29,26 +29,7 @@ client.on("message", async message => {
   const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
   
- if(command === "avatar") {
-        let usr = message.mentions.users.first() ? message.mentions.users.first() : message.author;
-    if (usr){
-      message.channel.send({
-        embed: {
-          color: 3447003,
-          author: {
-              name: `${bot.user.username}`,
-              icon_url: `${bot.user.avatarURL}`
-          },
-          title: `${usr.username}'s Avatar!`,
-          image: {
-            url: usr.avatarURL,
-          },
-      },
-      })
-    } else {
-      message.channel.send(':x: Please provide a user fr me to search for!')
-    }
-}
+ 
 	
   if(command === "ping") {
     const m = await message.channel.send("Ping?");
