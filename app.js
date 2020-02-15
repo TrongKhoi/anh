786b7abe586046575a860e5d7f2ed296037bd5ba
TrongKhoi <40121111+TrongKhoi@users.noreply.github.com>
2020-02-15 23:18:35 +0700

    Update app.js

--- app.js
+++ app.js
@@ -188,10 +188,14 @@ const embed = new Discord.RichEmbed()
 
 
 client.on("message", (message) => {
-  if (message.content.startsWith("Hiện hình")) {client.user.setStatus(`online`);}
-  if (message.content.startsWith("Ẩn thân")) {client.user.setStatus(`invisible`);}
-  if (message.content.startsWith("Afk")) {client.user.setStatus(`idle`);}
-  if (message.content.startsWith("Bận")) {client.user.setStatus(`dnd`);}
+  if (message.content.startsWith("Bot Online")) {client.user.setStatus(`online`);}
+  if (message.content.startsWith("Bot Offline")) {client.user.setStatus(`invisible`);}
+  if (message.content.startsWith("Bot Afk")) {client.user.setStatus(`idle`);}
+  if (message.content.startsWith("Bot Busy")) {client.user.setStatus(`dnd`);}
+  if (message.content.startsWith("sever japan")) {
+	  guild.setRegion('Japan')
+ 	  .then(g => console.log(`Updated guild region to ${g.region}`))
+ 	  .catch(console.error);;}	
 	
   if (message.content.startsWith("meo meo")) {message.channel.send("Gâu gâu");}
   if (message.content.startsWith("chào mn")) {message.channel.send("Mn chào em");}
