801a336a1f240bfdc4eb36966764af4780f47161
TrongKhoi <40121111+TrongKhoi@users.noreply.github.com>
2020-03-08 21:58:48 +0700

    Update app.js

--- app.js
+++ app.js
@@ -246,12 +246,10 @@ client.on("message", (message) => {
   }
  
 if (message.content.startsWith("ảnh Lan")) {
-	  if(message.author.id == oid){
 	  number = 1;
 	  var imageNumber = Math.floor(Math.random()*(number - 1 + 1)) + 1;
 	  message.channel.send({files: ["./anh/Lan/" + imageNumber + ".jpg"]});
 	  }
-	  else { message.reply("Error! DM Admin to fix command");}
   }
 	
   if (message.content.startsWith("ảnh Nhung")) {
